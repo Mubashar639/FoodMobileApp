b0f1baef1119317ea8bed4652945b53ea6116b3b
AbdulRehman197 <abdulrehman19761@gmail.com>
2019-11-25 17:20:16 +0500

    adding dropdown lib

--- components/Bording2Component.js
+++ components/Bording2Component.js
@@ -1,78 +1,107 @@
-import React from 'react';
-import{View,StyleSheet,Image,TouchableOpacity,Alert} from 'react-native'
-import {Container, Content, Button, Text,Center } from 'native-base';
-import * as Font from 'expo-font';
-
+import React from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Alert,ScrollView } from "react-native";
+import { Container, Content, Button, Text, Center } from "native-base";
+import DropDownItem from "react-native-drop-down-item";
 // assets
 //import bording1Image from '../assets/images/On Boarding 1.png'
 
-
-const Bording2  = (props) =>{
-   return(
-    <View style = {styles.container}>
-        <Image source = {require('../assets/images/OnBoarding2.png')} style =      {styles.backgroundImage}> 
-        </Image>
-        <Button  small  bordered light style = {styles.skipButton} >
-             <Text></Text>
-         </Button>
-        <Button onPress = {()=>props.navigation.navigate('Bording3')} style = {styles.button} bordered light large  ><Text></Text></Button>
-     </View>
- )
+class Bording2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contents: [
+        {
+          title: "Title 1",
+          body: "Hi. I love this component. What do you think?"
+        },
+        {
+          title: "See this one too",
+          body: "Yes. You can have more items."
+        },
+        {
+          title: "Thrid thing",
+          body:
+            "What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text? What about very long text?"
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+        <View style={styles.container}>
+        <ScrollView style={{ alignSelf: 'stretch' }}>
+          {
+            this.state.contents
+              ? this.state.contents.map((param, i) => {
+                return (
+                  <DropDownItem
+                    key={i}
+                    // style={styles.dropDownItem}
+                    contentVisible={false}
+                    // invisibleImage={IC_ARR_DOWN}
+                    // visibleImage={IC_ARR_UP}
+                    header={
+                      <View>
+                        <Text style={{
+                          fontSize: 16,
+                          color: 'blue',
+                        }}>{param.title}</Text>
+                      </View>
+                    }
+                  >
+                    <Text>
+                      {param.body}
+                    </Text>
+                  </DropDownItem>
+                );
+              })
+              : null
+          }
+          <View style={{ height: 96 }}/>
+        </ScrollView>
+      </View>
+    );
+  }
 }
-       
+
 //styles
-       
-              
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        
-    },
-    buttonContainer:{
-          width:250,
-        height:70,
-        backgroundColor:' rgb(253,244,226)',
-         position:'relative',
-        left:130,
-        bottom:25,
-    },
-    button:{
-        marginTop:760,
-        bottom:35,
-        width:200,
-       
-        left:140
-        
-      
-    },
-    skipButton:{
-        width:100,
-        top:45,
-        left:370
-        
-    },
-    buttonText:{
-        color:'rgb(58,70,149)',
-        textAlign:'center',
-        marginLeft:30
-    },
-//   rgb(253,244,226)
-    backgroundImage :{
-     width:'100%',
-     height:'100%',
-     resizeMode: 'stretch',
-     position:'absolute',
+  container: {
+    flex: 1
+  },
+  buttonContainer: {
+    width: 250,
+    height: 70,
+    backgroundColor: " rgb(253,244,226)",
+    position: "relative",
+    left: 130,
+    bottom: 25
+  },
+  button: {
+    marginTop: 760,
+    bottom: 35,
+    width: 200,
 
-
-    }
+    left: 140
+  },
+  skipButton: {
+    width: 100,
+    top: 45,
+    left: 370
+  },
+  buttonText: {
+    color: "rgb(58,70,149)",
+    textAlign: "center",
+    marginLeft: 30
+  },
+  //   rgb(253,244,226)
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "stretch",
+    position: "absolute"
+  }
 });
 
-       
-       
-       
-       
 export default Bording2;
-       
-       
-       
--- components/IntroComponent.js
+++ components/IntroComponent.js
@@ -1,83 +1,252 @@
-import React from 'react';
-import{View,StyleSheet,Image,TouchableOpacity,Alert} from 'react-native'
-import {Container, Content, Button, Text,Center } from 'native-base';
-import Swiper from 'react-native-swiper'
+import React, { useState } from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import { Container, Content, Button, Text, Center } from "native-base";
+import Swiper from "react-native-swiper";
 // assets
 //import bording1Image from '../assets/images/On Boarding 1.png'
 
+const Introduction = props => {
+  const [indexNo, setIndexNo] = useState(0);
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Swiper
+          dotStyle={styles.dotStyle}
+          activeDotStyle={styles.activeDotStyle}
+          style={styles.wrapper}
+          index={indexNo}
+          onIndexChanged={index => {
+            setIndexNo(index);
+          }}
+          showsButtons={indexNo == 2 ? false : true}
+          buttonWrapperStyle={{
+            backgroundColor: "transparent",
+            position: "absolute",
+            top: 300,
+            left: -200
+            // flex: 1,
+          }}
+          prevButton={<Text></Text>}
+          nextButton={
+            <View style={styles.nextButtonContainer}>
+              <Text style={styles.buttonText}>Next</Text>
+            </View>
+          }
+        >
+          {/* slide1 */}
+          <View style={styles.slide1}>
+            {/* topText */}
+            <View style={styles.topTextContainer}>
+              <Button
+                onPress={() => Alert.alert("ther")}
+                style={styles.skipButton}
+                small
+                transparent
+              >
+                <Text>skip</Text>
+              </Button>
+              <Text style={styles.topText}>
+                L.I.T can help you easily and conveniently find transportation
+                to visit your incarcerated loved ones
+              </Text>
+            </View>
+            {/* image */}
+            <View style={styles.imageStyle}>
+              <Image
+                source={require("../assets/images/OnBoardingE1.png")}
+                style={styles.backgroundImage}
+              />
+            </View>
+            {/* bottom text */}
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.bottomText}>
+                When a loved one becaome incarcerated it not only affects them,
+                but also may be an overwhelming time for the family. You want to
+                help your loved one as much as you can during this difficult
+                time but may not know where to start
+              </Text>
 
-const Introduction  = (props) =>{
-   return(
-    <View style = {styles.container}>
-    <Swiper style={styles.wrapper} showsButtons={false}>
-        <View style={styles.slide1}>
-         <Image source = {require('../assets/images/OnBoardingE1.png')} style =      {styles.backgroundImage} /> 
-         <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
-      </Swiper>
+              <Button
+                // onPress={() => setIndexNo(indexNo + 1)}
+                style={styles.conButton}
+                bordered
+                rounded
+              >
+                <Text>continue</Text>
+              </Button>
+            </View>
+          </View>
+          {/* slide2 */}
+          <View style={styles.slide2}>
+            {/* topText */}
+            <View style={styles.topTextContainer}>
+              <Button
+                onPress={() => Alert.alert("ther")}
+                style={styles.skipButton}
+                small
+                transparent
+              >
+                <Text>skip</Text>
+              </Button>
+              <Text style={styles.topText}>
+                Bring a little bit of home & hope
+              </Text>
+            </View>
+            {/* image */}
+            <View style={styles.imageStyle}>
+              <Image
+                source={require("../assets/images/OnBoardingE2.png")}
+                style={styles.backgroundImage}
+              />
+            </View>
+            {/* bottom text */}
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.bottomText}>
+                With Our Care Package you can Conveniently puchase fresh fruits.
+                vegetables,prepared foods, canned goods,snacks,candy & drinks to
+                give your loved one one your visit
+              </Text>
+
+              <Button
+                // onPress={() => setIndexNo(indexNo + 1)}
+                style={styles.conButton}
+                bordered
+                rounded
+              >
+                <Text>continue</Text>
+              </Button>
+            </View>
+          </View>
+          {/* slide3 */}
+          <View style={styles.slide3}>
+            {/* topText */}
+            <View style={styles.topTextContainer}>
+              <Text style={styles.topText}>
+                Punctual, Professional & Polite
+              </Text>
+            </View>
+            {/* image */}
+            <View style={styles.imageStyle}>
+              <Image
+                source={require("../assets/images/OnBoardingE3.png")}
+                style={styles.backgroundImage}
+              />
+            </View>
+            {/* bottom text */}
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.bottomText}>
+                L.I.T professional carriers will hand pick your order, Package
+                and deliver it to you! Hassle free
+              </Text>
+                <Button
+                  onPress={() => props.navigation.navigate("Bording2")}
+                  style={styles.conButton}
+                  bordered
+                  rounded
+                >
+                  <Text>get started</Text>
+                </Button>
+              
+            </View>
+          </View>
+        </Swiper>
+      </View>
     </View>
+  );
+};
 
- )
-}
-       
 //styles
-       
-       
-const styles = StyleSheet.create({
-    container:{
-        width:'100%',
-        height:300,
-        top:300
 
-    },
-     wrapper: {
-        width:'100%',
-        height:50
-     },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgb(253,244,226)"
+  },
+  wrapper: {},
   slide1: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(253,244,226)"
+  },
+  imageStyle: {
+    top: -25,
+    width: "100%",
+    height: 270
+  },
+  skipButton: {
+    top: -20,
+    position: "absolute",
+    right: 0
+  },
+  conButton: {
+    left: 150,
+    paddingLeft: 17,
+    width: 150,
+    position: "absolute",
+    bottom: -15
+  },
+  nextButtonContainer: {
+    width: 150,
+    height: 50,
+    left: 40,
+    marginTop: 150,
+    backgroundColor: "red",
+    opacity: 0
   },
   slide2: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(253,244,226)"
   },
   slide3: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgb(253,244,226)"
   },
-  text: {
-    color: '#fff',
+  dotStyle: {
+    top: -250
+  },
+  activeDotStyle: {
+    backgroundColor: "#2e3c91",
+    top: -250
+  },
+  topTextContainer: {
+    position: "absolute",
+    height: 230,
+    top: 20,
+    width: "95%",
+    padding: 40,
+    marginTop: 50,
+    marginLeft: 30
+  },
+  bottomTextContainer: {
+    position: "absolute",
+    height: 230,
+    top: 520,
+    width: "95%",
+    padding: 40,
+    marginTop: 50,
+    marginLeft: 30
+  },
+  topText: {
+    color: "#2e3c91",
     fontSize: 30,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
-    backgroundImage :{
-     width:'100%',
-     height:'100%',
-     resizeMode: 'stretch',
-     position:'absolute',
-
-
-    }
+  bottomText: {
+    color: "#2e3c91",
+    fontSize: 20
+    // fontWeight: "bold"
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "stretch",
+    position: "absolute"
+  }
 });
 
-       
-       
-       
-       
 export default Introduction;
-       
-       
-       
